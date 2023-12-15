7abe6c3eb218ce8918d494f9db84aff1b069ed8d
namoong0917 <namoong0917@gmail.com>
2023-12-15 19:58:16 +0900

    Refactor: Lifecycle 제어하기 - useEffect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import "./App.css";
+import Lifecycle from "./Lifecycle";
 
 function App() {
   const [data, setData] = useState([]);
@@ -38,6 +39,7 @@ function App() {
 
   return (
     <div className="App">
+      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
